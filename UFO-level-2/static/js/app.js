ee5128cbd0a1344a1b622120e555c380bc7e9d88
Shawn Williams <swillia75@gmail.com>
2020-08-18 09:55:29 -0400

    committing after modifiying html pages

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,4 @@
-// from data.js
+// bring in data from data.js
 
 var ufoData = data;
 
@@ -6,16 +6,22 @@ var ufoData = data;
 
 var tbody = d3.select("tbody");
 
-// append rows to table
 
+// loop through data print to console.log
 ufoData.forEach(function(ufoSitings) {
     console.log(ufoSitings);
+
+    // append rows to table
      var row = tbody.append("tr");
+
+     // loop through objects to get keys and values
      Object.entries(ufoSitings).forEach(function([key, value]) {
      console.log(key, value);
-  //     // Append a cell to the row for each value
-  //     // in the weather report object
+    // Append a cell to the row for each value
+    // in the weather report object
       var cell = row.append("td");
+
+      // add values to cells
       cell.text(value);
      });
   });
