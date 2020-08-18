99086b631224c9b6a114ac439ceaf7f1f104da44
Shawn Williams <swillia75@gmail.com>
2020-08-18 12:05:16 -0400

    added filter coding

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,5 @@
-// bring in data from data.js
+//Add data to webpage using javascript and d3
+// Assign data from data.js to a vaiable
 
 var ufoData = data;
 
@@ -25,3 +26,39 @@ ufoData.forEach(function(ufoSitings) {
       cell.text(value);
      });
   });
+
+//Listening for events and filter to search data by date
+
+//Select button
+var button = d3.select("#button")
+
+//Select form group
+
+var form = d3.select("#form-group")
+
+//create event handlers
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete event handler function
+
+function runEnter(){
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+   // Select the input element and get the raw HTML node
+   var inputElement = d3.select("#form_control");
+
+   // Get the value property of the input element
+   var inputValue = inputElement.property("value");
+ 
+   console.log(inputValue);
+   console.log(ufoData);
+ 
+   var ufofilteredData = ufoData.filter(ufoData => ufoData.date === inputValue);
+ 
+   console.log(ufofilteredData);
+
+};
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -25,3 +25,39 @@ ufoData.forEach(function(ufoSitings) {
       cell.text(value);
      });
   });
+
+  //Listening for events and filter to search data by date
+
+//Select button
+var button = d3.select("#button")
+
+//Select form group
+
+var form = d3.select("#form-group")
+
+//create event handlers
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete event handler function
+
+function runEnter(){
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+   // Select the input element and get the raw HTML node
+   var inputElement = d3.select("#form_control");
+
+   // Get the value property of the input element
+   var inputValue = inputElement.property("value");
+ 
+   console.log(inputValue);
+   console.log(ufoData);
+ 
+   var ufofilteredData = ufoData.filter(ufoData => ufoData.date === inputValue);
+ 
+   console.log(ufofilteredData);
+
+};
